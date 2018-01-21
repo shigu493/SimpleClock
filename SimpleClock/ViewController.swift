510d8dc21f6212367ae11ad1d2e7803b833bf3cb
misato.imaji <m.imaji1127@gmail.com>
2018-01-21 22:38:55 +0900

    Initial Commit

---
+++ SimpleClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleClock
+//
+//  Created by 今治 美里 on 2018/01/21.
+//  Copyright © 2018年 misato.imaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
