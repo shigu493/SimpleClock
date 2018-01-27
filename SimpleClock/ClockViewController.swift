0091837048b41c671631a548dfc12f28c3b3981a
misato.imaji <misato.imaji@imaji-MacBook-Pro.local>
2018-01-28 00:05:03 +0900

    タイマーで時計機能実装

--- SimpleClock/ClockViewController.swift
+++ SimpleClock/ClockViewController.swift
@@ -9,22 +9,34 @@
 import UIKit
 
 class ClockViewController: UIViewController {
-
+    
     @IBOutlet weak var lblTime: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // タイマーをセット
+        let timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(displayClock), userInfo: nil, repeats: true)
+        // 発火
+        timer.fire()
+        
         lblTime.font = UIFont(name: "Toma Slab", size: 50)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         
     }
-
-    override func viewWillAppear(_ animated: Bool) {
-
-    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func displayClock() {
+        // 現在時刻を「HH:MM:SS」形式で取得する
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        let displayTime = formatter.string(from: Date())    // Date()だけで現在時刻を表す
+        lblTime.text = displayTime
+    }
 }
-
