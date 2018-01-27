5d4a272d2a935dd3ded0cbf1f9fcde62104576ec
misato.imaji <misato.imaji@imaji-MacBook-Pro.local>
2018-01-27 23:57:18 +0900

    フォントファイル追加

--- SimpleClock/ClockViewController.swift
+++ SimpleClock/ClockViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ClockViewController.swift
 //  SimpleClock
 //
 //  Created by 今治 美里 on 2018/01/21.
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ClockViewController: UIViewController {
 
+    @IBOutlet weak var lblTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblTime.font = UIFont(name: "Toma Slab", size: 50)
+        
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
